9d11b00e03501e9cd6b24b5e4593675142b3b9e7
Chris Masters <ccm22_@outlook.com>
2022-10-12 18:10:46 +1100

    commit tree 526755ec1a60909979a2f061329ea1cf32cf2f04

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// Created a function called addingEventListener(). 
+// Inside the parent function, the element with id = "button" is grabbed. 
+// Grabbed the element that we want to add the event listener to and save a reference to it in the input variable.
+// Call addEventListener() on that element to tell JavaScript to listen for the event.
+// Pass two arguments to addEventListener(): the name of the event to listen for (in this case, click) and a callback function that will be executed when the event is "heard."
+
 function addingEventListener() {
+   
     const input = document.getElementById('button');
 
 function clickAlert() {
